ca5001b83f934cc9f675cb5e28de27b6ebd1a15c
v-280 <vrindabansal06@gmail.com>
2024-12-04 01:21:15 +0530

    Frontend Done!!

---
+++ Frontend/src/components/Course.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Cards from "./Cards";
+import list from "../../public/list.json";
+import { Link } from "react-router-dom";
+
+function Course() {
+  return (
+    <>
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-28 items-center justify-center text-center">
+          <h1 className="text-2xl  md:text-4xl">
+            We're delighted to have you{" "}
+            <span className="text-pink-500">Here! :)</span>
+          </h1>
+
+          <p className="mt-12">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi
+            beatae veniam inventore itaque officiis voluptas fugiat ex nihil
+            nemo nesciunt aperiam ipsum, neque illum cupiditate facilis
+            provident alias quas vitae maxime voluptatum voluptatem magnam nulla
+            eveniet? Ex aut quidem corrupti!
+          </p>
+          <Link to="/">
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+              Back
+            </button>
+          </Link>
+        </div>
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
+          {list.map((item) => (
+            <Cards key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Course;
---
+++ Frontend/src/home/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Banner from "../components/Banner";
+import Freebok from "../components/Freebok";
+import Footer from "../components/Footer";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Banner />
+      <Freebok />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
